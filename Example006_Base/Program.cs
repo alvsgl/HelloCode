c0976afce24a113be3fee1ad126533fb22e6d434
alvsgl <alvsgl@ya.ru>
2023-02-02 19:11:02 +0300

    add Example 006 Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,10 @@
+﻿int[5] values = {1, 2, 6, 8, 4};
+int max = 0;
+Console.Write("values = ");
+foreach (int value in values)
+{
+    Console.Write(value + " ");
+    if(value > max) max = value;
+}
+
+Console.WriteLine("\nmax = " + max);
