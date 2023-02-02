351cb15275b7ee6b5fe59731347396e664306a1c
alvsgl <alvsgl@ya.ru>
2023-02-02 18:59:04 +0300

    add Example 005 if else

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Веедите ваше имя ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "вася") {
+    Console.Write("Здорово, Васян, дружище!");
+} else {
+    if (username == "") {
+        username = "Анонимус";
+    }
+    Console.Write("Привет, " + username + "!");
+}
