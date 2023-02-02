4245915aae98873421b69f31acb9ad40391642e1
alvsgl <alvsgl@ya.ru>
2023-02-02 18:09:00 +0300

    add Example 002 Hello User

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Веедите ваше имя ");
+string username = Console.ReadLine();
+if (username == "") {
+    username = "Анонимус";
+}
+Console.Write("Привет, ");
+Console.Write(username + "!");
